23321357c56e779028325e827491e3a15c109df1
Carter <carteralzamora@gmail.com>
2025-02-17 12:04:09 -0800

    it works to change the color based off the key! - just the rgb values are weird rn

--- draw.py
+++ draw.py
@@ -27,24 +27,65 @@ context.fill()
 #])
 
 myDict = {'Blue': (550, 1000, 1100),
-          'Purple': (520, 560, 1300),
-          'Pink': (1200, 1400, 1450)}
+          'Red': (520, 560, 1300),
+          'Green': (1200, 1400, 1450)}
 
 
 #iterate through the dict by color to pull each starting point individually 
 # this lmost works jsut cant figure out how to access the key iteratively to define the colors
-for x_coordinates in myDict.values():
-    for x in x_coordinates:
-        print(myDict.keys())
-        print(x)
-        print("")
+# for x_coordinates in myDict.values():
+#     for x in x_coordinates:
+#         print(myDict.keys())
+#         print(x)
+#         print("")
+#         context.set_line_width(5)
+#         context.move_to(x,150)        #(x,y) START POINT
+#         context.line_to(x, 50)       #END POINT
+#         if myDict.keys() == 'Blue':
+#             context.set_source_rgb(0,0,225)
+        # elif myDict.keys() == 'Purple':
+        #     context.set_source_rgb(160,32,240)
+        # elif myDict.keys() == 'Pink':
+        #     context.set_source_rgb(225,192,203)
+        # context.stroke()
+
+
+#iterate through the dictionary by the key (color val) and the value (x_coord)
+for color_val, x_coord in myDict.items():
+    #index the value toupke so that you can go through each coordinate and pplot it one by one
+    for i, coord in enumerate(x_coord):
+
+        # print(coord)
+        # print(f"{color_val}: {x_coord[i]}")
         context.set_line_width(5)
-        context.move_to(x,150)        #(x,y) START POINT
-        context.line_to(x, 50)       #END POINT
-        if myDict.keys() == 'Blue':
+        context.move_to(x_coord[i],150)        #(x,y) START POINT
+        context.line_to(x_coord[i], 50) 
+        if color_val == 'Red':
+            context.set_source_rgb(255, 0, 0)
+            print("PURPLE")
+        if color_val == 'Blue':
             context.set_source_rgb(0,0,225)
-        elif myDict.keys() == 'Purple':
-            context.set_source_rgb(160,32,240)
-        elif myDict.keys() == 'Pink':
-            context.set_source_rgb(225,192,203)
+            print("BLUE")
+        elif color_val == 'Green':
+            context.set_source_rgb(0, 255, 0)
+            print("PINK")
         context.stroke()
+
+
+    #print("")
+    # for x_coordinates in myDict.values():
+    #     for x in x_coordinates:
+    #         # print(x)
+    #         # print("")
+            # context.set_line_width(5)
+            # context.move_to(x,150)        #(x,y) START POINT
+            # context.line_to(x, 50) 
+    #         if color_val == 'Blue':
+    #             context.set_source_rgb(0,0,225)
+    #             print("BLUE")
+    #         elif color_val == 'Pink':
+    #             print("PINK")
+    #             context.set_source_rgb(160,32,240)
+    #         elif color_val == 'Purple':
+    #             print("PURPLE")
+    #             context.set_source_rgb(225,192,203)
