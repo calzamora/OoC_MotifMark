416328d5a411154fe953ff29da584c222e6dc6fc
Carter <carteralzamora@gmail.com>
2025-03-06 16:36:33 -0800

    everything but legend and cleaning

--- OoC_practice/draw.py
+++ OoC_practice/draw.py
@@ -31,24 +31,6 @@ myDict = {'Blue': (550, 1000, 1100),
           'Green': (1200, 1400, 1450)}
 
 
-#iterate through the dict by color to pull each starting point individually 
-# this lmost works jsut cant figure out how to access the key iteratively to define the colors
-# for x_coordinates in myDict.values():
-#     for x in x_coordinates:
-#         print(myDict.keys())
-#         print(x)
-#         print("")
-#         context.set_line_width(5)
-#         context.move_to(x,150)        #(x,y) START POINT
-#         context.line_to(x, 50)       #END POINT
-#         if myDict.keys() == 'Blue':
-#             context.set_source_rgb(0,0,225)
-        # elif myDict.keys() == 'Purple':
-        #     context.set_source_rgb(160,32,240)
-        # elif myDict.keys() == 'Pink':
-        #     context.set_source_rgb(225,192,203)
-        # context.stroke()
-
 
 #iterate through the dictionary by the key (color val) and the value (x_coord)
 for color_val, x_coord in myDict.items():
@@ -71,22 +53,3 @@ for color_val, x_coord in myDict.items():
             context.set_source_rgb(0, 255, 0)
             print("PINK")
         context.stroke()
-
-
-    #print("")
-    # for x_coordinates in myDict.values():
-    #     for x in x_coordinates:
-    #         # print(x)
-    #         # print("")
-            # context.set_line_width(5)
-            # context.move_to(x,150)        #(x,y) START POINT
-            # context.line_to(x, 50) 
-    #         if color_val == 'Blue':
-    #             context.set_source_rgb(0,0,225)
-    #             print("BLUE")
-    #         elif color_val == 'Pink':
-    #             print("PINK")
-    #             context.set_source_rgb(160,32,240)
-    #         elif color_val == 'Purple':
-    #             print("PURPLE")
-    #             context.set_source_rgb(225,192,203)
--- motif-mark-oop.py
+++ motif-mark-oop.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import re
 import math
 import cairo
 import argparse
@@ -8,85 +8,233 @@ def get_args():
     parser = argparse.ArgumentParser(description="test")
     parser.add_argument("-m", help="motif list file") #type: str
     parser.add_argument("-f", help="fasta file") #type: str
-    parser.add_argument("-o", help="output fig prefex") #type: str
+    # parser.add_argument("-o", help="output fig prefex") #type: str
     return parser.parse_args()
 
 args = get_args()
-
 motif = args.m 
 fasta = args.f 
-comp_dict = {'A':'T', 'T':'A', 'C':'G', 'G':'C','N': 'N', 'a':'t', 't':'a', 'c':'g', 'g':'c', 'n':'n'}
+# out_pic = args.o
 
-#define function to reverse compliment 
-def rev_comp(seq: str) -> str:
-    '''this function returns a reverse copliments a sequence of upper case bases including Ns'''
-    rev_seq = seq[::-1]
-    RC = ""
-    for base in rev_seq: 
-        RC += comp_dict[base]
-    return(RC)
+####################
+# GLOBAL VARIABLES #
+####################
 
-# define the motif  class 
-# will hold the motif and have a function to generate the possible variations
-# color assigned to that motif 
-# list of start locations for that motif
-class MotifSeq: 
-    def __init__(self, motif):
-        self.motif = motif
+#define line metrics
+line_width = 1
+#define feature variables 
+feature_height = 25
+#color list for drawing motifs: 
+# list of 5 tupols containg RGB values divided 255
+#pink, purple, light blue, green, orange
+COLOT_LIST = [(255/255, 96/255, 208/255),
+              (160/255, 32/255, 255/255),
+              (80/255, 208/255, 255/255),
+              (96/255, 255/255, 128/255),
+              (255/255, 106/255, 16/255)]
+#motif variations dict key: given motif, value: regex motif
+motif_dict = {}
+#key: given letter value: regex possible letters for motifs
+iupac ={
+    "U":"[TU]",
+    "W":"[ATU]",
+    "S":"[CG]",
+    "M":"[AC]",
+    "K":"[GTU]",
+    "R":"[AG]",
+    "Y":"[CTU]",
+    "B":"[CGTU]",
+    "D":"[AGTU]",
+    "H":"[ACTU]",
+    "V":"[ACG]",
+    "N":"[ACGTU]",
+}
 
-# define fasta record class 
-# hold  holds the intron and exon locations 
-# hold total length of record
-# hold header 
-class record:
-    def __init__(self, intron_start, intron_end, exon_start, exon_end, total_len):
-        self.intron_start = intron_start
-        self.intron_end = intron_end
-        self.exon_start = exon_start
-        self.exon_end = exon_end
-        self.total_len = len(self)
+##################
+# DEFINE CLASSES #
+##################
+ 
+class Motif: 
+    def __init__(self, start, length, gene_number, color):
+        self.start = start
+        self.length = length 
+        self.gene_number = gene_number
+        self.color = color
+    #draw motif
+    def draw(self, context):
+        context.set_source_rgb(self.color[0], self.color[1], self.color[2])        #set color to black
+        y = (gene_num*100) + 137.5
+        x = self.start + 50
+        length = self.length
+        height = feature_height
+        context.rectangle(x,y,length,height)
+        context.fill()
+
+class Gene: 
+    def __init__(self, length, gene_name, gene_number):
+        self.length = length 
+        self.gene_name = gene_name
+        self.gene_number = gene_number
+
+
+    def draw(self, context):
+        #draw the line
+        y = (self.gene_number *100) + 150
+        context.set_source_rgb(0,0,0)        #set color to black 
+        end_point = self.length + 50
+        context.set_line_width(line_width)      #Line density
+        context.move_to(50,y)        #(x,y) START POINT
+        context.line_to(end_point,y)       #END POINT
+        context.stroke()                #DRAW
+
+        #draw header 
+        title_height = (self.gene_number * 100) + 100
+        context.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+        context.set_font_size(10)
+        context.move_to(50, title_height)
+        context.show_text(self.gene_name)
+
+
+class Exon: 
+    def __init__(self, start, length, gene_number):
+        self.start = start
+        self.length = length
+        self.gene_number = gene_number
+
+    def draw(self, context):
+        context.set_source_rgb(0,0,0)        #set color to black
+        y = (gene_num*100) + 137.5
+        x = self.start + 50
+        length = self.length
+        height = feature_height
+        context.rectangle(x,y,length,height)
+        context.fill()
+
+#############
+# FUNCTIONS #
+#############
+
+#def find and replace the regex expressions of the
+def reg_ex_replace(motif) -> str: 
+    '''Takes in motif and outputs seq containing possible variations in regex'''
+    # print(motif)
+    motif = motif.upper()
+    for char in motif: 
+        if char in iupac:
+            motif = motif.replace(char, iupac[char])
+    return(motif)
+
+#function to find the exon start and length
+def find_exon(sequence: str) -> tuple:
+    '''Takes a DNA seq and returns the start position and total length'''
+    for i in range(len(sequence)):
+        if sequence[i].isupper():
+            start = int(i)
+            break
+    for i in range(start, len(sequence)):
+        if sequence[i].islower():
+            end = int(i)
+            break
+    length = int(end) - int(start)
+    return (start, length)
 
 # open the fasta file and read in the fasta records: 
-
-with (open(fasta, "r") as fasta):
-    header = ''
-    seq = ''
-    intron_start = 0
-    exon_loc = []
-    for line in fasta: 
+#fasta dict: header = key seq = value 
+fasta_dict = {}
+with (open(fasta, "r") as fasta_file,
+      open (motif, "r") as motif_file):
+    #OPEN MOTIF FILE AND POP MOTIF DICT
+    for motif in motif_file: 
+        motif = motif.strip()
+        # print(motif)
+        new_motif = reg_ex_replace(motif)
+        # print(new_motif)
+        motif_dict[motif] = new_motif
+    # print(motif_dict)
+    #
+    for line in fasta_file: 
         #set the header and seq variables equal to the header and seq of that record 
         if line.startswith(">"):
             header = line.strip()
+            fasta_dict[header] = ""
         else: 
-            seq += line.strip()
-        # reverse compliment the sequence if needed 
-        if "reverse complement" in header: 
-            seq = rev_comp(seq)
-            # print(seq)
-        elif "reverse complement" not in header: 
-            seq = seq
-        #find exon location
-        for i, character in enumerate(seq):
-            # print(seq)
-            # print(i)
-            # print(character)
-            if character.isupper() == True:
-                # print("exon found")
-                # print(header)
-                # print(character)
-                # print(i)
-                exon_loc.append(i)
+            fasta_dict[header] += line.strip()
+    #iterate through fasta dict to find and draw genes
 
-            # print(exon_loc)
-                exon_start = exon_loc[0]
-                exon_end = exon_loc[-1]
-                print(header)
-                print(exon_start)
-                print(exon_end)
+##########################
+# CREATE CANVAS + LEGEND #
+##########################
 
-            # elif c
+height = (len(fasta_dict) * 100) + 100
+length = 0 
+for header in fasta_dict:
+    if len(fasta_dict[header]) > length:
+        length = len(fasta_dict[header]) 
+length += 250
+# print(f"the length of the canvaas is:{length}\n the height is {height}")
+
+#set the coordinates of the canvas
+surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, length, height)
+#make the backgroud
+context = cairo.Context(surface)
+context.set_source_rgb(1,1,1)
+context.paint()
+
+# TITLE: 
+title_height = 25
+context.set_source_rgb(0,0,0)
+context.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+context.set_font_size(20)
+context.move_to(350, title_height)
+context.show_text("Motif Locations Within 4 Genes")
+
+#LEGEND:
 
 
-        
+################################
+# CREATE GENE + EXONS + MOTIFS #
+################################
+
+for i, header in enumerate(fasta_dict):
+    #FIND OVERALL GENE INFO: 
+    gene_name = header
+    gene_num = i 
+    gene_length =  len(fasta_dict[header])
+    gene = Gene(gene_length,gene_name,gene_num)
+    gene.draw(context)
+
+    #FIND EXON INFO: 
+    exon_touple = (find_exon(fasta_dict[header]))
+    exon_start = exon_touple[0]
+    exon_len = exon_touple[1]
+    gene_num = i
+    exon = Exon(exon_start, exon_len, gene_num)
+    exon.draw(context)
 
 
+    #ITERATE THROUGH FASTA SEQ TO FIND MOTIFS: 
+    #MAKE SEQ ALL UPPER CASE:
+    seq = fasta_dict[header].upper()
+    # print(seq)
+    for j, motif in enumerate(motif_dict):
+        # print(motif_dict[motif])
+        matches = re.finditer(motif_dict[motif],seq)
+        for match in matches:
+            # print(match.start(), match.group())
+            # motif_name = motif
+            start = match.start()
+            length = len(match.group())
+            gene_number = i
+            motif = Motif(start, length, gene_number, COLOT_LIST[j])
+            motif.draw(context)
+            # print(motif)
+
+
+
+
+
+#add legend and title 
+
+#save to png 
+name = fasta.split(".")
+surface.write_to_png(f"{name[0]}.png")
